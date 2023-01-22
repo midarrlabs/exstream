3fd608cc272349c5808cfad66aa2b261bf8bf2b3
trueChazza <chaz.goodare@gmail.com>
2023-01-22 23:22:00 +1300

    refactor: rename sample video

--- test/exstream_test.exs
+++ test/exstream_test.exs
@@ -2,7 +2,7 @@ defmodule ExstreamTest do
   use ExUnit.Case
 
   test "it should have packet" do
-    assert Exstream.probe("support/sample__1080__libx264__aac__30s__video.mkv")
+    assert Exstream.probe("support/sample_1080_libx264_aac_30s_video.mkv")
     |> Enum.at(0) === %{"flags" => "K_", "pos" => "2563", "pts_time" => "0.054000"}
   end
 end
