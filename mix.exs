5c1e7d83ed1c11968202a7146b3a817c4fe33215
trueChazza <chaz.goodare@gmail.com>
2023-01-22 23:13:13 +1300

    build: add ex_doc

--- mix.exs
+++ mix.exs
@@ -7,6 +7,11 @@ defmodule Exstream.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      description: "Pluggable video streams",
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/midarrlabs/exstream"}
+      ],
       deps: deps()
     ]
   end
@@ -21,8 +26,7 @@ defmodule Exstream.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:jason, "~> 1.4"}
     ]
   end
