a1d5a2e65739f0f8716302421bfa883accb8abeb
trueChazza <chaz.goodare@gmail.com>
2023-01-27 12:33:27 +1300

    refactor: merge modules

--- lib/exstream.ex
+++ lib/exstream.ex
@@ -1,4 +1,5 @@
 defmodule Exstream do
+  import Plug.Conn
 
   def get_result({result, 0}) do
     result
@@ -76,5 +77,34 @@ defmodule Exstream do
     ])
 
     "#{ chunk_id }.mp4"
+  end
+
+  def handle_range({"range", "bytes=0-1"}, conn, path, file_size) do
+    conn
+    |> put_resp_header("content-range", "bytes 0-1/#{file_size}")
+    |> send_file(206, path, 0, 2)
+  end
+
+  def handle_range({"range", "bytes=" <> start_pos}, conn, path, file_size) do
+    offset = String.split(start_pos, "-")
+             |> List.first()
+             |> String.to_integer()
+
+    conn
+    |> put_resp_header("content-range", "bytes #{offset}-#{file_size - 1}/#{file_size}")
+    |> send_file(206, path, offset, file_size - offset)
+  end
+
+  def handle_range(nil, conn, path, file_size) do
+    conn
+    |> put_resp_header("content-range", "bytes 0-#{file_size - 1}/#{file_size}")
+    |> send_file(206, path, 0, file_size)
+  end
+
+  def video(conn, path) do
+    {:ok, %{size: file_size}} = File.stat(path)
+
+    List.keyfind(conn.req_headers, "range", 0)
+    |> handle_range(conn |> put_resp_header("content-type", "video/mp4"), path, file_size)
   end
 end
--- test/exstream_test.exs
+++ test/exstream_test.exs
@@ -1,5 +1,6 @@
 defmodule ExstreamTest do
   use ExUnit.Case
+  use Plug.Test
 
   @path "support/sample_1080_libx264_aac_30s_video.mkv"
 
@@ -39,5 +40,40 @@ defmodule ExstreamTest do
     assert Exstream.chunk_video(@path, 2818447, some_random_string) === "#{ some_random_string }.mp4"
 
     File.rm("#{ some_random_string }.mp4")
+  end
+
+  test "it should start" do
+
+    conn = conn(:get, "/")
+           |> Exstream.video(@path)
+
+    assert conn.status === 206
+    assert conn.state === :file
+    assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+    assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-16351753/16351754"})
+  end
+
+  test "it should seek" do
+
+    conn = conn(:get, "/")
+           |> put_req_header("range", "bytes=124-")
+           |> Exstream.video(@path)
+
+    assert conn.status === 206
+    assert conn.state === :file
+    assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+    assert Enum.member?(conn.resp_headers, {"content-range", "bytes 124-16351753/16351754"})
+  end
+
+  test "it should Safari probe" do
+
+    conn = conn(:get, "/")
+           |> put_req_header("range", "bytes=0-1")
+           |> Exstream.video(@path)
+
+    assert conn.status === 206
+    assert conn.state === :file
+    assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+    assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-1/16351754"})
   end
 end
