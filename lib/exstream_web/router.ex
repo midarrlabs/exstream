5e20fccb7fad0b9e41c7cfde7e3088a7d44f62b5
trueChazza <chaz.goodare@gmail.com>
2023-01-27 10:48:51 +1300

    feat: add playground video

--- lib/exstream_web/router.ex
+++ lib/exstream_web/router.ex
@@ -10,6 +10,12 @@ defmodule Exstream.Router do
     |> send_file(200, Path.absname("lib/exstream_web/index.html"))
   end
 
+  get "/video" do
+    %Plug.Conn{req_headers: headers} = conn
+
+    Exstream.SendVideo.send_video(conn, headers, Path.absname("support/sample_1080_libx264_aac_30s_video.mkv"))
+  end
+
   match _ do
     send_resp(conn, 404, "Oops!")
   end
