78c1a0e30ab5060b6b86ea57e3bda6ffee77ec1b
trueChazza <chaz.goodare@gmail.com>
2023-02-24 10:08:45 +1300

    refactor: inline static playlist

--- lib/exstream/playlist.ex
+++ lib/exstream/playlist.ex
@@ -4,32 +4,8 @@ defmodule Exstream.Playlist do
 
   @type t :: %__MODULE__{url: String.t, duration: String.t}
 
-  def get_header() do
-    "#EXTM3U\n"
-  end
-
-  def get_type() do
-    "#EXT-X-PLAYLIST-TYPE:VOD\n"
-  end
-
-  def get_duration() do
-    "#EXT-X-TARGETDURATION:10\n"
-  end
-
-  def get_version() do
-    "#EXT-X-VERSION:4\n"
-  end
-
-  def get_sequence() do
-    "#EXT-X-MEDIA-SEQUENCE:0\n"
-  end
-
   def get_step(duration, step, base_url) do
     "#EXTINF:#{ Exstream.get_one_tenth(duration) },\n#{ base_url }&segment=#{ Exstream.get_step(duration, step) }\n"
-  end
-
-  def get_end() do
-    "#EXT-X-ENDLIST"
   end
 
   def parse_duration(7, duration) do
@@ -66,11 +42,11 @@ defmodule Exstream.Playlist do
 
     duration = Exstream.probe(path) |> Exstream.get_duration()
 
-    get_header() <>
-    get_type() <>
-    get_duration() <>
-    get_version() <>
-    get_sequence() <>
+    "#EXTM3U\n" <>
+    "#EXT-X-PLAYLIST-TYPE:VOD\n" <>
+    "#EXT-X-TARGETDURATION:10\n" <>
+    "#EXT-X-VERSION:4\n" <>
+    "#EXT-X-MEDIA-SEQUENCE:0\n" <>
     get_step(duration, 0, base_url) <>
     get_step(duration, 1, base_url) <>
     get_step(duration, 2, base_url) <>
@@ -81,6 +57,6 @@ defmodule Exstream.Playlist do
     get_step(duration, 7, base_url) <>
     get_step(duration, 8, base_url) <>
     get_step(duration, 9, base_url) <>
-    get_end()
+    "#EXT-X-ENDLIST"
   end
 end
--- test/exstream/playlist_test.exs
+++ test/exstream/playlist_test.exs
@@ -2,34 +2,6 @@ defmodule Exstream.Playlist.Test do
   use ExUnit.Case
 
   @video "priv/video.mkv"
-  
-  test "it should get header" do
-    assert Exstream.Playlist.get_header() === "#EXTM3U\n"
-  end
-
-  test "it should get type" do
-    assert Exstream.Playlist.get_type === "#EXT-X-PLAYLIST-TYPE:VOD\n"
-  end
-  
-  test "it should get duration" do
-    assert Exstream.Playlist.get_duration() === "#EXT-X-TARGETDURATION:10\n"
-  end
-  
-  test "it should get version" do
-    assert Exstream.Playlist.get_version === "#EXT-X-VERSION:4\n"
-  end
-
-  test "it should get sequence" do
-    assert Exstream.Playlist.get_sequence === "#EXT-X-MEDIA-SEQUENCE:0\n"
-  end
-
-  test "it should get end" do
-    assert Exstream.Playlist.get_end === "#EXT-X-ENDLIST"
-  end
-
-  test "it should build" do
-    assert Exstream.Playlist.build(@video, "/some/base/url?token=some-token") === "#EXTM3U\n#EXT-X-PLAYLIST-TYPE:VOD\n#EXT-X-TARGETDURATION:10\n#EXT-X-VERSION:4\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:3,\n/some/base/url?token=some-token&segment=0\n#EXTINF:3,\n/some/base/url?token=some-token&segment=3\n#EXTINF:3,\n/some/base/url?token=some-token&segment=6\n#EXTINF:3,\n/some/base/url?token=some-token&segment=9\n#EXTINF:3,\n/some/base/url?token=some-token&segment=12\n#EXTINF:3,\n/some/base/url?token=some-token&segment=15\n#EXTINF:3,\n/some/base/url?token=some-token&segment=18\n#EXTINF:3,\n/some/base/url?token=some-token&segment=21\n#EXTINF:3,\n/some/base/url?token=some-token&segment=24\n#EXTINF:3,\n/some/base/url?token=some-token&segment=27\n#EXT-X-ENDLIST"
-  end
 
   test "it should parse invalid duration" do
     assert Exstream.Playlist.parse_duration("1:40:30") === "01:40:30"
@@ -49,5 +21,9 @@ defmodule Exstream.Playlist.Test do
 
   test "it should get duration hour" do
     assert Exstream.Playlist.get_duration_hour("1:40:30") === 1
+  end
+
+  test "it should build" do
+    assert Exstream.Playlist.build(@video, "/some/base/url?token=some-token") === "#EXTM3U\n#EXT-X-PLAYLIST-TYPE:VOD\n#EXT-X-TARGETDURATION:10\n#EXT-X-VERSION:4\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:3,\n/some/base/url?token=some-token&segment=0\n#EXTINF:3,\n/some/base/url?token=some-token&segment=3\n#EXTINF:3,\n/some/base/url?token=some-token&segment=6\n#EXTINF:3,\n/some/base/url?token=some-token&segment=9\n#EXTINF:3,\n/some/base/url?token=some-token&segment=12\n#EXTINF:3,\n/some/base/url?token=some-token&segment=15\n#EXTINF:3,\n/some/base/url?token=some-token&segment=18\n#EXTINF:3,\n/some/base/url?token=some-token&segment=21\n#EXTINF:3,\n/some/base/url?token=some-token&segment=24\n#EXTINF:3,\n/some/base/url?token=some-token&segment=27\n#EXT-X-ENDLIST"
   end
 end
