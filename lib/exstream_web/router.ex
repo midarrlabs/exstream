bab27f81d6868a078f7037f70a2250417dfb1918
trueChazza <chaz.goodare@gmail.com>
2023-02-24 12:59:04 +1300

    feat: add segment route

--- lib/exstream_web/router.ex
+++ lib/exstream_web/router.ex
@@ -4,27 +4,27 @@ defmodule Exstream.Router do
   plug(:match)
   plug(:dispatch)
 
-  @video "test/fixtures/video.mkv"
-  @audio "test/fixtures/audio.mp3"
+  @video "priv/video.mkv"
 
-  get "/watch" do
+  get "/" do
     conn
     |> put_resp_content_type("text/html")
     |> send_file(200, Path.absname("lib/exstream_web/index.html"))
   end
 
-  get "/segments/:step" do
-    conn
-    |> send_resp(200, Exstream.segment(@video, String.to_integer(step)))
+  get "/segment:query" do
+    %{"start" => start, "end" => finish} = fetch_query_params(conn).query_params
+
+    Exile.stream!(~w(ffmpeg -copyts  -ss #{ start } -i #{ @video } -c copy -to #{ finish } -f mpegts pipe:1))
+    |> Enum.into(
+         conn
+         |> send_chunked(200)
+       )
   end
 
   get "/playlist.m3u8" do
     conn
-    |> send_resp(200, Exstream.Playlist.build(%Exstream.Playlist{duration: 30, url: "/some/base/url?token=some-token"}))
-  end
-
-  get "/audio.mp3" do
-    Exstream.Range.stream(%Exstream.Range{conn: conn, path: Path.absname(@audio)})
+    |> send_resp(200, Exstream.Playlist.build(%Exstream.Playlist{duration: "00:00:30", url: "/segment?token=some-token"}))
   end
 
   match _ do
