800ef1f806c24c99eb6dee7309ed711c2f046757
trueChazza <chaz.goodare@gmail.com>
2023-02-24 15:46:45 +1300

    refactor: refactor duration

--- test/exstream/duration_test.exs
+++ test/exstream/duration_test.exs
@@ -1,23 +1,23 @@
 defmodule Exstream.Duration.Test do
   use ExUnit.Case
 
-  test "it should parse invalid duration" do
-    assert Exstream.Duration.parse_duration("1:40:30") === "01:40:30"
+  test "it should parse invalid" do
+    assert Exstream.Duration.parse("1:40:30") === "01:40:30"
   end
 
-  test "it should parse duration" do
-    assert Exstream.Duration.parse_duration("01:40:30") === "01:40:30"
+  test "it should parse" do
+    assert Exstream.Duration.parse("01:40:30") === "01:40:30"
   end
 
-  test "it should get duration second" do
-    assert Exstream.Duration.get_duration_second("1:40:30") === 30
+  test "it should get second" do
+    assert Exstream.Duration.get_second("1:40:30") === 30
   end
 
-  test "it should get duration minute" do
-    assert Exstream.Duration.get_duration_minute("1:40:30") === 40
+  test "it should get minute" do
+    assert Exstream.Duration.get_minute("1:40:30") === 40
   end
 
-  test "it should get duration hour" do
-    assert Exstream.Duration.get_duration_hour("1:40:30") === 1
+  test "it should get hour" do
+    assert Exstream.Duration.get_hour("1:40:30") === 1
   end
 end
