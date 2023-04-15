342fae090ddacb4e3f5f7e98b86bc2d311182641
trueChazza <chaz.goodare@gmail.com>
2023-04-15 16:20:41 +1200

    refactor: refactor stream chunks to 5 seconds
    closes #14

--- lib/exstream_web/controllers/playlist_controller.ex
+++ lib/exstream_web/controllers/playlist_controller.ex
@@ -2,17 +2,17 @@ defmodule ExstreamWeb.PlaylistController do
   use ExstreamWeb, :controller
 
     defp get_extinf(url, path, duration) do
-      steps = floor((Extaima.parse(duration) |> Extaima.seconds()) / 10)
+      steps = floor((Extaima.parse(duration) |> Extaima.seconds()) / 5)
 
       Enum.map_join(1..steps, fn step ->
-        "#EXTINF:10,\n#{url}?path=#{path}&start=#{step * 10 - 10}&end=#{step * 10}\n"
+        "#EXTINF:5,\n#{url}?path=#{path}&start=#{step * 5 - 5}&end=#{step * 5}\n"
       end)
     end
 
     def build(url, path, duration) do
       "#EXTM3U\n" <>
         "#EXT-X-PLAYLIST-TYPE:VOD\n" <>
-        "#EXT-X-TARGETDURATION:10\n" <>
+        "#EXT-X-TARGETDURATION:5\n" <>
         "#EXT-X-VERSION:4\n" <>
         "#EXT-X-MEDIA-SEQUENCE:0\n" <>
         get_extinf(url, path, duration) <>
